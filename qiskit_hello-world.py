c47ebf124017e7049b8c502c6ae505683d3b85ba
Swapna Sindhu Mishra <48241258+Swapna-Sindhu-Mishra@users.noreply.github.com>
2024-08-15 06:17:30 +0200

    Extend to n-qubit

--- qiskit_hello-world.py
+++ qiskit_hello-world.py
@@ -1,3 +1,9 @@
+# %% [markdown]
+# # Hello world example on a 2-qubit bell state
+# 
+# ## 1. Map problem to circuits and operators
+
+# %%
 from qiskit import QuantumCircuit
 
 qc = QuantumCircuit(2)
@@ -6,7 +12,7 @@ qc.h(0)
 qc.cx(0,1)
 qc.draw(output='mpl')
 
-
+# %%
 from qiskit.quantum_info import Pauli
 
 ZZ = Pauli('ZZ')
@@ -18,7 +24,10 @@ IX = Pauli('IX')
 
 observables = (ZZ, ZI, IZ, XX, XI, IX)
 
+# %% [markdown]
+# ## 2. Execute on backend
 
+# %%
 from qiskit_aer.primitives import Estimator
 
 estimator = Estimator()
@@ -27,7 +36,10 @@ job = estimator.run([qc]*len(observables), observables)
 
 job.result()
 
+# %% [markdown]
+# ## 3. Post-process and plot
 
+# %%
 import matplotlib.pyplot as plt
 
 data = ['ZZ', 'ZI', 'IZ', 'XX', 'XI', 'IX']
@@ -36,4 +48,81 @@ values = job.result().values
 plt.plot(data, values, '-o')
 plt.xlabel('Observables')
 plt.ylabel('Expectation value')
+plt.show()
+
+# %% [markdown]
+# # Extend Hellow World example to an $n$-qubit GHZ state
+# 
+# ## 1. Map the problem to circuits and operators
+
+# %%
+def get_qc_for_n_qubit_GHZ_state(n):
+    qc = QuantumCircuit(n)
+    qc.h(0)
+    for i in range(n-1):
+        qc.cx(i, i-1)
+    return qc
+
+n = 10
+qc = get_qc_for_n_qubit_GHZ_state(n)
+qc.draw(output='mpl')
+
+# %%
+from qiskit.quantum_info import SparsePauliOp
+
+operator_strings = ['Z' + 'I' * i + 'Z' + 'I' * (n-2-i) for i in range(n-1)]
+print(operator_strings)
+print(len(operator_strings))
+
+operators = [SparsePauliOp(operator_string) for operator_string in operator_strings]
+
+# %% [markdown]
+# ## 2. Optimize for quantum execution
+
+# %%
+from qiskit_ibm_runtime import QiskitRuntimeService
+from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+
+backend = QiskitRuntimeService().least_busy(operational=True, simulator=False)
+pass_manager = generate_preset_pass_manager(optimization_level=1, backend=backend)
+
+qc_transpiled = pass_manager.run(qc)
+operators_transpiled_list = [op.apply_layout(qc_transpiled.layout) for op in operators]
+
+# %% [markdown]
+# ## 3. Execute on the backend
+
+# %%
+from qiskit_ibm_runtime import EstimatorV2 as Estimator
+from qiskit_ibm_runtime import EstimatorOptions
+
+options = EstimatorOptions()
+options.resilience_level = 1
+options.optimization_level = 0
+options.dynamical_decoupling.sequence_type = "XY4"
+
+estimator = Estimator(backend, options=options)
+job = estimator.run([(qc_transpiled, operators_transpiled_list)])
+job_id = job.job_id()
+print(job_id)
+
+# %% [markdown]
+# ## 4. Post-process and plot
+
+# %%
+job_id = 'ctyqw6g5yns0008s6yng'
+service = QiskitRuntimeService()
+job = service.job(job_id)
+
+import matplotlib.pyplot as plot
+
+data = list(range(1, len(operators)+1))
+result = job.result()[0]
+values = result.data.evs
+values = [v / values[0] for v in values]
+
+plt.scatter(data, values, marker='o', label='100-qubit GHZ state')
+plt.xlabel('Distance between qubits $i$')
+plt.ylabel(r'\langle Z_0 Z_i \rangle / \langle Z_0 Z_1 \rangle$')
+plt.legend()
 plt.show()
